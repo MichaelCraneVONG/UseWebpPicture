380eb6edca5f952956c1dfe078edf5f1b5bf0f6e
书呆子Crane <MichaelCraneVONG>
2016-03-18 13:24:28 +0800

    Initial Commit

---
+++ UseWebpPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseWebpPicture
+//
+//  Created by myApple on 16/3/18.
+//  Copyright © 2016年 crane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UseWebpPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UseWebpPicture
+//
+//  Created by myApple on 16/3/18.
+//  Copyright © 2016年 crane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
